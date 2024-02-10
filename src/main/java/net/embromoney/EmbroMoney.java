49722f941e8caf0fce64e7e537ab278e62387c28
forengeit345 <timophey2907@gmail.com>
2024-02-10 23:35:55 +0500

    Initial commit

---
+++ src/main/java/net/embromoney/EmbroMoney.java
@@ -0,0 +1,16 @@
+package net.embromoney;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EmbroMoney implements ModInitializer {
+	public static final String MOD_ID = "embromoney";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
